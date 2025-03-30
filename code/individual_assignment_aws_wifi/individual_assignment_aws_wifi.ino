c2f72b67e744f87f1d9d768bf08ecd83e577b4be
Angelo-Pio <angelopompeo0@gmail.com>
2025-03-30 15:37:10 +0200

    connection to aws using wifi

--- code/individual_assignment_aws_wifi/individual_assignment_aws_wifi.ino
+++ code/individual_assignment_aws_wifi/individual_assignment_aws_wifi.ino
@@ -1,13 +1,58 @@
+
+
 #include <arduinoFFT.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/queue.h>
 
+#define COMMUNICATION_METHOD 2  // 1 for WiFi-MQTT 2 for LoraWan-TTN
+
+#if COMMUNICATION_METHOD == 1
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <NetworkClientSecure.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
+#include "secrets.h"
+
+// WiFi connection variables
+const char* ssid = "Angelo";
+const char* password = "Pompeo00";
+const char* mqttServer = AWS_IOT_ENDPOINT;  //ip address of smartphone
+int port = 8883;
+const char* topic = "angelo/signal";
+char clientId[50];
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+#elif COMMUNICATION_METHOD == 2
+
+##include <LoRa.h>
+
+#include "ttn.h"
+
+
+#define LORA_RST 14        // LoRa Reset pin
+#define LORA_MOSI 27       // LoRa MOSI pin
+#define LORA_MISO 19       // LoRa MISO pin
+#define LORA_SCK 5        // LoRa SCK pin
+#define LORA_CS 18         // LoRa CS pin
+
+//#define LORA_IRQ 26        // LoRa IRQ pin
+
+// TTN credentials
+const char * appEui = JOINEUI;   // Replace with your AppEUI
+const char * appKey = APPKEY;   // Replace with your AppKey
+const char * devEui = DEVEUI;   // Replace with your DevEUI
+
+#endif
+
+
 // Signal ggeneration variables
 const uint16_t samples = 64;                 // This value MUST ALWAYS be a power of 2
-const double old_samplingFrequency = 22000;  // Sampling frequency in Hz
+const double old_samplingFrequency = 11;  // Sampling frequency in Hz
 const double best_samplingFrequency = NULL;  // Sampling frequency in Hz
 const double signalFrequency1 = 3;           // Frequency of first sine wave (3 Hz)
 const double signalFrequency2 = 5;           // Frequency of second sine wave (5 Hz)
@@ -33,16 +78,7 @@ QueueHandle_t transmission_queue;
 unsigned long sampleCount = 0;
 unsigned long startTime = 0;
 
-// WiFi connection variables
-const char* ssid = "Angelo";
-const char* password = "Pompeo00";
-const char* mqttServer = "192.168.56.34";  //ip address of smartphone
-int port = 1883;
-const char* topic = "angelo/signal";
-char clientId[50];
 
-WiFiClient espClient;
-PubSubClient client(espClient);
 
 
 
@@ -67,8 +103,8 @@ void setup() {
 
   analogReadResolution(12);
 
+#if COMMUNICATION_METHOD == 1
   //Connection to MQTT broker
-  
   Serial.println("WiFi connection setup");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -76,8 +112,20 @@ void setup() {
     Serial.println(".");
   }
   Serial.println("WiFi connection established");
+
+  Serial.println(AWS_IOT_ENDPOINT);
+
+  espClient.setCACert(AWS_CERT_CA);
+  espClient.setCertificate(AWS_CERT_CRT);
+  espClient.setPrivateKey(AWS_CERT_PRIVATE);
+
   client.setServer(mqttServer, port);
+
+#elif COMMUNICATION_METHOD == 2
   
+ 
+
+#endif
 
   //Find max sampling frequency v1
   //startTime = millis();
@@ -97,44 +145,39 @@ void setup() {
   xTaskCreate(generate_signal_task, "generate_signal_task", 4096, NULL, 1, NULL);
   xTaskCreate(sampling_signal_task, "sampling_signal_task", 4096, NULL, 1, NULL);
   xTaskCreate(avg_transmission_task, "avg_transmission_task", 4096, NULL, 1, NULL);
-
 }
 
 void loop() {
-  //find_max_freq();
 
-  //send_over_wifi_mqtt(2.0);
 }
 
-void avg_transmission_task(void* pvParameters){
+void avg_transmission_task(void* pvParameters) {
 
   double avg_freq;
   char msg[100];
 
-  while(1){
-    if(xQueueReceive(transmission_queue, &avg_freq, (TickType_t) 5)){
+  while (1) {
+    if (xQueueReceive(transmission_queue, &avg_freq, pdMS_TO_TICKS(2000))) {
 
       Serial.printf("Received message to publish on queue trasmission_queue: %.2f \n", avg_freq);
+#if COMMUNICATION_METHOD == 1
       send_over_wifi_mqtt(avg_freq);
-
+#endif
     }
-
   }
-
-
 }
 
 void sampling_signal_task(void* pvParameters) {
 
   float sum = 0;
   double sample;
-  float received_samples[WINDOW_SIZE] = { 0 };  
-  int pos = 0;                                  
+  float received_samples[WINDOW_SIZE] = { 0 };
+  int pos = 0;
   int count = 0;
 
 
-    while (1) {
-    if (xQueueReceive(samples_queue, &sample, (TickType_t)5)) {
+  while (1) {
+    if (xQueueReceive(samples_queue, &sample, pdMS_TO_TICKS(2000))) {
 
       //find_best_sampling_freq();
       received_samples[pos] = sample;
@@ -154,6 +197,7 @@ void sampling_signal_task(void* pvParameters) {
 
 
 void generate_signal_task(void* pvParameters) {
+  
   /*
   double ratio1 = twoPi * signalFrequency1 / old_samplingFrequency;  // Fraction of cycle for first sine wave
   double ratio2 = twoPi * signalFrequency2 / old_samplingFrequency;  // Fraction of cycle for second sine wave
@@ -164,19 +208,42 @@ void generate_signal_task(void* pvParameters) {
 
 
     Serial.printf("Sample number %d : %lf \n", i, vReal[i]);
-  }*/
+  }
+
+  // Apply windowing (optional, can reduce spectral leakage)
+  FFT.windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+
+  // Compute the FFT (forward FFT)
+  FFT.compute(FFT_FORWARD);
+
+  
+
+  // Convert complex FFT result to magnitude (for frequency analysis)
+  FFT.complexToMagnitude();
+
+  // Output the frequency spectrum (peak frequencies)
+  double peakFrequency1 = FFT.majorPeak();  // Find the dominant frequency in the FFT result
+  Serial.print("Dominant Frequency: ");
+  Serial.println(peakFrequency1, 6);  // Print the peak frequency detected
+  */
+  
   double val = 6.0;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
   while (1) {
 
-    for(int i = 0; i < 50 ; i++){
-        
-        val = (double) i ;
+    for (int i = 0; i < 50; i++) {
+
+      val = (double)i;
 
 
-        xQueueSend(samples_queue, &val, (TickType_t)0);
-        delay(5000);
+      xQueueSend(samples_queue, &val, (TickType_t)0);
+      vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(5000));  // We use vTaskDelayUntil to get more precise delay rate
     }
   }
+
+  while(1){}
+  delay(2000);
 }
 
 
@@ -208,7 +275,7 @@ void find_best_sampling_freq() {
   Serial.printf("Peak Frequency: %.2f Hz\n", x);
 }
 
-
+#if COMMUNICATION_METHOD == 1
 void mqttReconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
@@ -225,19 +292,18 @@ void mqttReconnect() {
     }
   }
 }
-void send_over_wifi_mqtt(double val){
+void send_over_wifi_mqtt(double val) {
 
   const int MSG_BUFFER_SIZE = 100;
   char msg[MSG_BUFFER_SIZE];
 
-  if(!client.connected()){
+  if (!client.connected()) {
     mqttReconnect();
   }
   client.loop();
 
-  snprintf(msg, MSG_BUFFER_SIZE,"%.2f",val);
-  Serial.printf("Publishing message: %.2f to topic %s \n", val,topic);
-  client.publish(topic,msg);
-
+  snprintf(msg, MSG_BUFFER_SIZE, "%.2f", val);
+  Serial.printf("Publishing message: %.2f to topic %s \n", val, topic);
+  client.publish(topic, msg);
 }
-
+#endif
